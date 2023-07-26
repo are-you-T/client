5908e62eca1470e72aa9a46f7b415152d281eb8f
[SW5] 이수민 <tnklr1111@gmail.com>
2023-07-27 01:03:03 +0900

    style: tailwind-styled-comoponents 적용중

--- src/components/TestCard.tsx
+++ src/components/TestCard.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import tw from "tailwind-styled-components";
+
 //@ts-ignore
 export default function TestCard({ contents }) {
   return (
-    <div className="w-80 py-9 px-4 mb-5 cursor-pointer text-center rounded-2xl bg-white">
-      <p>{contents}</p>
-    </div>
+    <Testcard>
+      <TestcardText>{contents}</TestcardText>
+    </Testcard>
   );
 }
+
+const Testcard = tw.div`;
+w-80
+py-9 
+px-4 
+mb-5 
+cursor-pointer 
+text-left 
+rounded-2xl 
+bg-white
+`;
+
+const TestcardText = tw.p`
+ml-2
+`;
