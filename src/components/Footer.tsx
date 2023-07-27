f3334b5ee2ba9a92ac1c30b0dc9c5e78f2c0aaac
rebi13 <rebi13@naver.com>
2023-07-28 01:22:08 +0900

    fix: 헤더, 푸터 스타일 수정. 추가수정 필요

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,18 @@ import tw from "tailwind-styled-components";
 import { BsList } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import Button from "./Button";
-import { ReactComponent as LogoSvg } from '../assets/logo.svg';
-import { ReactComponent as GitHubSvg } from '../assets/github.svg';
-import { ReactComponent as ShareSvg } from '../assets/share.svg';
+import { ReactComponent as LogoSvg } from "../assets/logo.svg";
+import { ReactComponent as GitHubSvg } from "../assets/github.svg";
+import { ReactComponent as ShareSvg } from "../assets/share.svg";
 
 export default function Foot() {
   return (
     <Footer>
       <FooterWrap>
-        <LogoSvg />
+        <LogoSvg className="text-sm" />
         <FooterTextArea>
           <FooterText>AYT Company</FooterText>
-          <FooterText>All content is provided for fun purposes only</FooterText>  
+          <FooterText>All content is provided for fun purposes only</FooterText>
           <FooterText>Copyright © 2023 - All right reserved</FooterText>
         </FooterTextArea>
 
@@ -31,18 +31,19 @@ export default function Foot() {
   );
 }
 
-
-
 const Footer = tw.footer`
+  flex
   bg-yellow-500
   w-full
   h-22
   flex-shrink-0
+  p-4
 `;
 
 const FooterWrap = tw.div`
     flex 
-    items-center 
+    items-center
+    justify-between 
 `;
 
 const FooterTextArea = tw.div`
@@ -52,14 +53,22 @@ h-12
 flex-col
 justify-center
 flex-shrink-0
-`
-const FooterLinkIcon = tw.div`
-w-40
-flex-col-reverse
+grow-[0.8]
 `;
+// flex-grow 0.8
+
+const FooterLinkIcon = tw.div`
+flex
+w-8
+flex-row
+`;
+
 const FooterLink = tw.a`
 w-auto
-`
+mr-3
+`;
+// mrgin right
+
 const FooterText = tw.div`
   text-xs
-`
+`;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,11 @@ import { Link } from "react-router-dom";
 import Button from "./Button";
 // import companyLogo from "../assets/logo.svg";
 import Logo from "./Logo";
-import { ReactComponent as LogoSvg } from '../assets/logo.svg';
+import { ReactComponent as LogoSvg } from "../assets/logo.svg";
 
 export default function Head() {
   return (
     <Header>
-      
       <Logo></Logo>
       <Drawer>
         <DrawerToggle id="my-drawer" type="checkbox"></DrawerToggle>
@@ -19,23 +18,30 @@ export default function Head() {
             <BsList></BsList>
           </DrawerButton>
         </DrawerContent>
-        <div className="drawer-side">
+        <DrawerSide>
           <DrawerOverlay htmlFor="my-drawer"></DrawerOverlay>
           <SideBarContent>
-            {"Sidebar content here"}
-            <SiderBarItem>Sidebar Item 1</SiderBarItem>
-            <SiderBarItem>Sidebar Item 2</SiderBarItem>
+            <Logo></Logo>
+            <SiderBarItem className="bg-regal-yellow">
+              테스트 하러가기
+            </SiderBarItem>
+            <SiderBarItem className="bg-black text-white">
+              통계 보러가기
+            </SiderBarItem>
+            <SiderBarItem className="bg-black text-white">
+              담벼락 보러가기
+            </SiderBarItem>
+            <SideBarFooter>
+              <SideBarFooterContent></SideBarFooterContent>
+              <SideBarFooterContent></SideBarFooterContent>
+            </SideBarFooter>
           </SideBarContent>
-        </div>
+        </DrawerSide>
       </Drawer>
     </Header>
-
   );
 }
 
-// <Logo></Logo>
-// <Hamburger></Hamburger>
-// <header className='w-full flex items-center justify-between p-4 text-2xl border-b border-zinc-600 mb-4'>
 const Header = tw.header`
   flex
   flex-wrap
@@ -43,10 +49,6 @@ const Header = tw.header`
   w-full
   h-auto
   shrink-0
-`;
-
-
-const Hamburger = tw.span`
 `;
 
 const Drawer = tw.span`
@@ -64,12 +66,19 @@ const DrawerButton = tw.label`
 const DrawerToggle = tw.input`
   drawer-toggle
 `;
+
 const DrawerOverlay = tw.label`
   drawer-overlay
 `;
+
 const DrawerContent = tw.div`
   drawer-content
 `;
+
+const DrawerSide = tw.div`
+  drawer-side
+`;
+
 const SideBarContent = tw.ul`
   menu
   p-4
@@ -77,22 +86,26 @@ const SideBarContent = tw.ul`
   h-full
   bg-base-200
   text-base-content
+  bg-regal-purple
 `;
-const SiderBarItem = tw.li``;
-{
-  /* <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    { Page content here }
 
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
-  </div>
-  <div className="drawer-side">
-    <label htmlFor="my-drawer" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
-      { Sidebar content here }
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
+const SiderBarItem = tw.li`
+  mt-10
+  btn
+  btn-wide
+  w-100
+  h-10
+  flex-shrink-0
+  rounded-tr-3xl
+  shadow-lg
+`;
 
-    </ul>
-  </div> */
-}
+const SideBarFooter = tw.div`
+`;
+
+const SideBarFooterContent = tw.div`
+`;
+// border-radius: 100px;
+// border: 1px solid rgba(0, 0, 0, 0.00);
+// background: #000;
+// box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
