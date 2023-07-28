d23ef80b9647dab4ef04b89ea0e4b3412ff6eca8
reib13 <rebi13@naver.com>
2023-07-28 14:01:53 +0900

    chore: 패키지 추가 및 tailwind 세팅 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  corePlugins: {
+    preflight: true,
+  },
   theme: {
     extend: {
       colors: {
         brand: "#329dfa",
-        "regal-yellow": "#FFDF3F",
-        "regal-purple": "#B2ACF9",
       },
       backgroundImage: {
         main: "url('../src/assets/main.svg')",
         "black-semi-transparent": "rgba(0,0,0,0.7)",
-
+        "regal-yellow": "#FFDF3F",
+        "regal-purple": "#B2ACF9",
       },
     },
   },
