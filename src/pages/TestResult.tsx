eb7a1b2fdbeede040485e01272d9bcf4e42fa717
[SW5] 김진규 <rlawlsrb16736@gmail.com>
2023-07-31 13:19:03 +0900

    style: 테스트 결과 페이지 내 검사 결과 수정

--- src/pages/TestResult.tsx
+++ src/pages/TestResult.tsx
@@ -23,9 +23,10 @@ export default function TestResult() {
           </ContentWrapper>
           <OtherMain>
             <OtherTitle>내 검사 결과</OtherTitle>
-            <TypePercentageBar title="에너지" type1="직관형" type2="현실주의형" percent1={80} percent2={20} bar="w-4/5" textColor="text-orange-500" barColor="bg-orange-500"/>
-            <TypePercentageBar title="본성" type1="사고형" type2="감정형" percent1={60} percent2={40} bar="w-3/5" textColor="text-green-500" barColor="bg-green-500"/>
-            <TypePercentageBar title="전술" type1="계획형" type2="탐색형" percent1={100} percent2={0} bar="w-5/5" textColor="text-purple-500" barColor="bg-purple-500"/>
+            <TypePercentageBar title="에너지" type1="내향형(I)" type2="외향형(E)" percent1={60} percent2={40} bar="w-3/5" textColor="text-red-500" barColor="bg-red-500"/>
+            <TypePercentageBar title="인식" type1="직관형(N)" type2="현실주의형(S)" percent1={80} percent2={20} bar="w-4/5" textColor="text-orange-500" barColor="bg-orange-500"/>
+            <TypePercentageBar title="본성" type1="사고형(T)" type2="감정형(F)" percent1={60} percent2={40} bar="w-3/5" textColor="text-green-500" barColor="bg-green-500"/>
+            <TypePercentageBar title="전술" type1="계획형(J)" type2="탐색형(P)" percent1={100} percent2={0} bar="w-5/5" textColor="text-purple-500" barColor="bg-purple-500"/>
           </OtherMain>
           <Buttons>
               <ButtonItem>다시하기</ButtonItem>
