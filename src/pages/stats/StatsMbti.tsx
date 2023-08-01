f06856aa4ce3f2b98950dab833017af3e06c9ff8
crud0626 <sisofiy6048@gmail.com>
2023-08-01 11:05:01 +0900

    feat: 하단 버튼 링크 처리

--- src/pages/stats/StatsMbti.tsx
+++ src/pages/stats/StatsMbti.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import tw from 'tailwind-styled-components';
 import Chart from 'react-apexcharts';
 import { ApexOptions } from 'apexcharts';
+import { Link } from 'react-router-dom';
 
 // 차트 컴포넌트
 interface ResultType {
@@ -113,7 +114,7 @@ const Container = tw.main`
     h-auto
 `;
 
-const FooterBtn = tw.button`
+const FooterBtn = tw(Link)`
     bg-[#FFDF3F]
     border-solid
     border-1
@@ -121,6 +122,7 @@ const FooterBtn = tw.button`
     px-[20px]
     py-[20px]
     rounded-[100px]
+    text-center
 `;
 
 function StatsMbti() {
@@ -137,15 +139,15 @@ function StatsMbti() {
                         fill="none"
                     >
                         <ellipse
-                        cx="23.536"
-                        cy="23.204"
-                        fill="#00B26E"
-                        rx="23.536"
-                        ry="23.204"
+                            cx="23.536"
+                            cy="23.204"
+                            fill="#00B26E"
+                            rx="23.536"
+                            ry="23.204"
                         />
                         <path
-                        fill="#000"
-                        d="M19.024 18.303a6.762 6.762 0 0 1 9.523-.042l-1.741 1.737a1.016 1.016 0 0 0 .718 1.733H32.935c.562 0 1.014-.452 1.014-1.014v-5.41a1.016 1.016 0 0 0-1.733-.719l-1.758 1.758c-3.703-3.656-9.667-3.643-13.348.043a9.414 9.414 0 0 0-2.232 3.542 1.352 1.352 0 0 0 2.549.9 6.694 6.694 0 0 1 1.598-2.528Zm-5.363 7.148v5.41a1.016 1.016 0 0 0 1.733.718l1.758-1.758c3.703 3.656 9.667 3.644 13.348-.042a9.443 9.443 0 0 0 2.236-3.538 1.352 1.352 0 0 0-2.549-.9 6.693 6.693 0 0 1-1.597 2.527 6.762 6.762 0 0 1-9.523.043l1.737-1.742a1.016 1.016 0 0 0-.719-1.733h-5.41c-.562 0-1.014.453-1.014 1.015Z"
+                            fill="#000"
+                            d="M19.024 18.303a6.762 6.762 0 0 1 9.523-.042l-1.741 1.737a1.016 1.016 0 0 0 .718 1.733H32.935c.562 0 1.014-.452 1.014-1.014v-5.41a1.016 1.016 0 0 0-1.733-.719l-1.758 1.758c-3.703-3.656-9.667-3.643-13.348.043a9.414 9.414 0 0 0-2.232 3.542 1.352 1.352 0 0 0 2.549.9 6.694 6.694 0 0 1 1.598-2.528Zm-5.363 7.148v5.41a1.016 1.016 0 0 0 1.733.718l1.758-1.758c3.703 3.656 9.667 3.644 13.348-.042a9.443 9.443 0 0 0 2.236-3.538 1.352 1.352 0 0 0-2.549-.9 6.693 6.693 0 0 1-1.597 2.527 6.762 6.762 0 0 1-9.523.043l1.737-1.742a1.016 1.016 0 0 0-.719-1.733h-5.41c-.562 0-1.014.453-1.014 1.015Z"
                         />
                     </svg>
                 </button>
@@ -158,8 +160,8 @@ function StatsMbti() {
                 </ol>
             </section>
             <div className="btns flex flex-col text-3xl text-black font-bold w-full m-auto">
-                <FooterBtn>MBTI 통계</FooterBtn>
-                <FooterBtn>담벼락 바로가기</FooterBtn>
+                <FooterBtn to="/stats">MBTI 통계</FooterBtn>
+                <FooterBtn to="/board">담벼락 바로가기</FooterBtn>
             </div>
         </Container>
     );
