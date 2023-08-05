cdad59f3742cc566e1ee73b4bb0ba6761d4ecc8c
minhyeok <minhyeokbaek100@gmail.com>
2023-08-06 01:02:32 +0900

    fix: stats수정

--- src/components/common/MbtiTypesModal.tsx
+++ src/components/common/MbtiTypesModal.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import tw from "tailwind-styled-components";
 
+const ModalBg = tw.div`
+w-[390px] absolute top-0 left-1/2 -translate-x-1/2 h-full backdrop-blur-sm bg-black/[.3]
+`;
+
+export { ModalBg };
+
 // MBTI 선택 모달
 function MbtiTypesModal({
   selectMbti,
@@ -149,9 +155,7 @@ function MbtiTypesModal({
         </MbtiList>
       </ul>
       {isButton && (
-        <MbtiButton onClick={() => onThisConfirm()}>
-          확인
-        </MbtiButton>
+        <MbtiButton onClick={() => onThisConfirm()}>확인</MbtiButton>
       )}
     </ModalWrap>
   );
