1e5c4c7eef2b509680db87761c619e4c19298afb
crud0626 <sisofiy6048@gmail.com>
2023-08-08 21:16:05 +0900

    refactor: MBTI별 통계페이지
    * NoData를 별도 컴포넌트로 분리 및 스타일 수정
    
    * MBTI 선택 모달 수정
    
    * 렌더링 로직 수정
    
    * ChartOptions 객체 컴포넌트 외부로 이동
    
    * 코드 복잡도 개선 작업

--- src/pages/stats/StatsMbti.tsx
+++ src/pages/stats/StatsMbti.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import tw from 'tailwind-styled-components';
 import { ApexOptions } from 'apexcharts';
 import Chart from 'react-apexcharts';
@@ -8,7 +8,7 @@ import axiosReq from '@/api';
 import { resData } from '@/interfaces';
 import LoadingIndicator from '@/components/common/LoadingIndicator';
 import Character from '@/components/common/Character';
-import MbtiTypesModal, { ModalBg } from '@/components/common/MbtiTypesModal';
+import MbtiTypesModal from '@/components/common/MbtiTypesModal';
 
 interface QuestionItem {
     idx: number;
@@ -28,13 +28,67 @@ interface MbtiStatsByType {
     mbtiData: QuestionItem[];
 }
 
-const Container = tw.main`
-    bg-black
-    text-white
-    p-[20px]
-    w-[390px]
-    min-h-[calc(100vh_-_170px)]
-    m-auto
+const chartOptions: ApexOptions = {
+    chart: {
+        type: 'bar',
+        height: "100px",
+        stacked: true,
+        stackType: '100%',
+        toolbar: { show: false }
+    },
+    plotOptions: {
+        bar: { horizontal: true },
+    },
+    stroke: {
+        width: 1,
+        colors: ['#fff']
+    },
+    tooltip: {
+        x: {
+            show: false
+        },
+        // @ts-ignore
+        y: {
+            formatter: (_, { seriesIndex, w }) => {
+                const percentage = Math.round(w.globals.seriesPercent[seriesIndex]);
+                return `${percentage}%`;
+            },
+            title: {
+                // @ts-ignore
+                formatter: (val) => `${val} : `,
+            }
+        }
+    },
+    xaxis: {
+        labels: { show: false },
+        axisBorder: { show: false },
+        axisTicks: { show: false }
+    },
+    yaxis: {
+        labels: { show: false },
+        axisBorder: { show: false },
+        axisTicks: { show: false }
+    },
+    fill: { opacity: 1 },
+    grid: { show: false },
+    legend: { show: false }
+};
+
+const Container = styled.main<{ isOpenModal?: boolean }>`
+    margin: auto;
+    padding: 20px;
+    position: relative;
+    width: 390px;
+    min-height: calc(100vh - 170px);
+    color: white;
+    background: black;
+
+    ${({ isOpenModal }) => 
+        isOpenModal && 
+        css`
+            overflow-y: hidden;
+            height: calc(100vh - 170px);
+    `}
 `;
 
 const FooterBtn = tw(Link)`
@@ -55,66 +109,11 @@ const ChartList = styled.ol`
     }
 `;
 
-function Footer({ mbtiType }: { mbtiType: string | undefined }) {
-    return (
-        <div className="btns flex flex-col text-3xl text-black font-bold w-full m-auto">
-            <FooterBtn to="/stats">MBTI 통계</FooterBtn>
-            <FooterBtn to={`/board/${mbtiType}`}>담벼락 바로가기</FooterBtn>
-        </div>
-    );
-}
-
 function ChartItem({ data }: { data: QuestionItem }) {
     const { idx, subject, answer, selection } = data;
 
     const [leftType, rightType] = Object.keys(selection);
     const [leftValue, rightValue] = Object.values(selection);
-
-    const chartOptions: ApexOptions = {
-        chart: {
-            type: 'bar',
-            height: "100px",
-            stacked: true,
-            stackType: '100%',
-            toolbar: { show: false }
-        },
-        plotOptions: {
-            bar: { horizontal: true },
-        },
-        stroke: {
-            width: 1,
-            colors: ['#fff']
-        },
-        tooltip: {
-            x: {
-                show: false
-            },
-            // @ts-ignore
-            y: {
-                formatter: (_, { seriesIndex, w }) => {
-                    const percentage = Math.round(w.globals.seriesPercent[seriesIndex]);
-                    return `${percentage}%`;
-                },
-                title: {
-                    // @ts-ignore
-                    formatter: (val) => `${val} : `,
-                }
-            }
-        },
-        xaxis: {
-            labels: { show: false },
-            axisBorder: { show: false },
-            axisTicks: { show: false }
-        },
-        yaxis: {
-            labels: { show: false },
-            axisBorder: { show: false },
-            axisTicks: { show: false }
-        },
-        fill: { opacity: 1 },
-        grid: { show: false },
-        legend: { show: false }
-    };
 
     const displaySeries: ApexAxisChartSeries = [
         { name: leftType, data: [leftValue] },
@@ -131,6 +130,27 @@ function ChartItem({ data }: { data: QuestionItem }) {
             </ul>
         </li>
     ); 
+}
+
+function NoData({ mbtiType }: { mbtiType: string | undefined }) {
+    return (
+        <main className='m-auto bg-black min-h-[calc(100vh_-_170px)] w-[390px] pt-16 flex flex-col justify-end'>
+            <Character bgcolor="#00B26E" gcolor="#FFA8DF" />
+            <div className='bg-[#00B26E] p-[20px] text-center grow'>
+                <h2 className='mb-8 text-6xl text-black font-bold'>No data</h2> 
+                <Footer mbtiType={mbtiType} />
+            </div>
+        </main>
+    );
+}
+
+function Footer({ mbtiType }: { mbtiType: string | undefined }) {
+    return (
+        <div className="btns flex flex-col text-3xl text-black font-bold w-full m-auto">
+            <FooterBtn to="/stats">MBTI 통계</FooterBtn>
+            <FooterBtn to={`/board/${mbtiType}`}>담벼락 바로가기</FooterBtn>
+        </div>
+    );
 }
 
 function StatsMbti() {
@@ -187,7 +207,7 @@ function StatsMbti() {
             );
 
             const filteredStats = filterStats(data);
-            // setStats(filteredStats);
+            setStats(filteredStats);
         } catch (error) {
             alert("데이터를 받아오던 중 에러가 발생했습니다.");
         } finally {
@@ -201,73 +221,55 @@ function StatsMbti() {
 
     if (!currMbti) {
         navigate('/');
+        return null;
     }
 
     if (isLoading) return <LoadingIndicator />;
 
-    if (!stats) {
-        return (
-            <Container className='p-0 pt-16 flex flex-col justify-end'>
-                <Character bgcolor={"#00B26E"} gcolor={"#FFA8DF"} />
-                <div className='bg-[#00B26E] p-[20px] text-center grow'>
-                    <h2 className='mb-8 text-6xl text-black font-bold'>No data</h2> 
-                    <Footer mbtiType={currMbti} />
-                </div>
-            </Container>
-        );
-    }
+    if (!stats) return <NoData mbtiType={currMbti} />;
 
     return (
-        <Container>
-            {stats && 
-                <>
-                {
-                    isOpenModal ?
-                    <>
-                        <ModalBg />
-                        <MbtiTypesModal
-                            selectMbti={mbtiType || []}
-                            onThisMbti={handleMbtiType}
-                            isButton={true}
-                            onThisConfirm={onChangeMbtiType}
+        <Container isOpenModal={isOpenModal}>
+            <div className="flex place-content-between items-center">
+                <h3 className="text-5xl font-bold">{stats.mbtiType}</h3>
+                {/* MBTI 변경 버튼 */}
+                <button onClick={() => setIsOpenModal(prev => !prev)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="48"
+                        height="47"
+                        fill="none"
+                    >
+                        <ellipse
+                            cx="23.536"
+                            cy="23.204"
+                            fill="#00B26E"
+                            rx="23.536"
+                            ry="23.204"
                         />
-                    </>
-                    : (
-                        <>
-                            <div className="flex place-content-between items-center">
-                                <h3 className="text-5xl font-bold">{stats.mbtiType}</h3>
-                                {/* MBTI 변경 버튼 */}
-                                <button onClick={() => setIsOpenModal(prev => !prev)}>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="48"
-                                        height="47"
-                                        fill="none"
-                                    >
-                                        <ellipse
-                                            cx="23.536"
-                                            cy="23.204"
-                                            fill="#00B26E"
-                                            rx="23.536"
-                                            ry="23.204"
-                                        />
-                                        <path
-                                            fill="#000"
-                                            d="M19.024 18.303a6.762 6.762 0 0 1 9.523-.042l-1.741 1.737a1.016 1.016 0 0 0 .718 1.733H32.935c.562 0 1.014-.452 1.014-1.014v-5.41a1.016 1.016 0 0 0-1.733-.719l-1.758 1.758c-3.703-3.656-9.667-3.643-13.348.043a9.414 9.414 0 0 0-2.232 3.542 1.352 1.352 0 0 0 2.549.9 6.694 6.694 0 0 1 1.598-2.528Zm-5.363 7.148v5.41a1.016 1.016 0 0 0 1.733.718l1.758-1.758c3.703 3.656 9.667 3.644 13.348-.042a9.443 9.443 0 0 0 2.236-3.538 1.352 1.352 0 0 0-2.549-.9 6.693 6.693 0 0 1-1.597 2.527 6.762 6.762 0 0 1-9.523.043l1.737-1.742a1.016 1.016 0 0 0-.719-1.733h-5.41c-.562 0-1.014.453-1.014 1.015Z"
-                                        />
-                                    </svg>
-                                </button>
-                            </div>
-                            <section>
-                                <ChartList className='mt-[40px]'>
-                                    {stats.mbtiData.map((data) => <ChartItem key={data.idx} data={data} />)}
-                                </ChartList>
-                            </section>
-                            <Footer mbtiType={currMbti} />
-                        </>
-                    )
-                }
-                </>
+                        <path
+                            fill="#000"
+                            d="M19.024 18.303a6.762 6.762 0 0 1 9.523-.042l-1.741 1.737a1.016 1.016 0 0 0 .718 1.733H32.935c.562 0 1.014-.452 1.014-1.014v-5.41a1.016 1.016 0 0 0-1.733-.719l-1.758 1.758c-3.703-3.656-9.667-3.643-13.348.043a9.414 9.414 0 0 0-2.232 3.542 1.352 1.352 0 0 0 2.549.9 6.694 6.694 0 0 1 1.598-2.528Zm-5.363 7.148v5.41a1.016 1.016 0 0 0 1.733.718l1.758-1.758c3.703 3.656 9.667 3.644 13.348-.042a9.443 9.443 0 0 0 2.236-3.538 1.352 1.352 0 0 0-2.549-.9 6.693 6.693 0 0 1-1.597 2.527 6.762 6.762 0 0 1-9.523.043l1.737-1.742a1.016 1.016 0 0 0-.719-1.733h-5.41c-.562 0-1.014.453-1.014 1.015Z"
+                        />
+                    </svg>
+                </button>
+            </div>
+            <section>
+                <ChartList className='mt-[40px]'>
+                    {stats.mbtiData.map((data) => <ChartItem key={data.idx} data={data} />)}
+                </ChartList>
+            </section>
+            <Footer mbtiType={currMbti} />
+            {
+                isOpenModal &&
+                <div className='mt-[-20px] ml-[-20px] bottom-0 absolute w-[390px] backdrop-blur-sm h-full'>
+                    <MbtiTypesModal
+                        selectMbti={mbtiType || []}
+                        onThisMbti={handleMbtiType}
+                        isButton={true}
+                        onThisConfirm={onChangeMbtiType}
+                    />
+                </div>
             }
         </Container>
     );
