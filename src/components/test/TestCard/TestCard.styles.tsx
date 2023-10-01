9d454980e7f570a82c15286e72a998118ea22fba
[SW5] 예은선 <ysvln0912@gmail.com>
2023-10-01 23:12:02 +0900

    style: media 에러 수정

--- src/components/test/TestCard/TestCard.styles.tsx
+++ src/components/test/TestCard/TestCard.styles.tsx
@@ -2,7 +2,7 @@ import tw from "twin.macro";
 
 export const Testcard = tw.button`
 w-80
-h-[120px]
+min-h-[120px]
 py-9
 px-4
 mb-5
@@ -11,19 +11,15 @@ text-left
 rounded-2xl
 bg-white
 text-black
-
-[@media(hover:hover){&:hover}]:bg-violet-400
-[@media(hover:hover){&:hover}]:border-transparent
-[@media(hover:hover){&:hover}]:-translate-y-1
-[@media(hover:hover){&:hover}]:text-white	
-
-active:translate-y+1
+hover:bg-violet-400
+hover:border-transparent
+hover:translate-y-1
+hover:text-white
+active:translate-y-1
 active:bg-purple-400
 active:text-white
-
 ease-in
 duration-200
-
 select-none
 relative
 animate-testComponent
