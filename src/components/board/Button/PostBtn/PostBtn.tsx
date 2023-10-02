8a8b190bd0e9f845441a67d73ba39fa673b6dd13
[SW5] 문수민 <sum95771@gmail.com>
2023-10-02 15:44:35 +0900

    design: postBtn 사이즈 수정

--- src/components/board/Button/PostBtn/PostBtn.tsx
+++ src/components/board/Button/PostBtn/PostBtn.tsx
@@ -12,11 +12,11 @@ export default function PostBtn({ setOpenBoardPost }: PostBtnProps) {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="47"
-        height="47"
+        width="46.41"
+        height="46.41"
         fill="none"
       >
-        <circle cx="23.5" cy="23.5" r="23.5" fill="#fff" />
+        <circle cx="23.205" cy="23.205" r="23.205" fill="#fff" />
         <path
           fill="#000"
           d="M21.913 35.217v-9.473H12.49V22.57h9.424v-9.424h3.174v9.424h9.473v3.174h-9.473v9.473h-3.174Z"
