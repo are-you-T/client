61246c8d4fd0804d842c56fa1e9866f59fca2d67
[SW5] 문수민 <sum95771@gmail.com>
2023-10-05 21:29:17 +0900

    feat: 카드상세페이지 경로 추가

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,29 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import App from "@/App";
-import Home from "@/pages/home/Home"
+import Home from "@/pages/home/Home";
 import NotFound from "@/pages/NotFound";
 import Test from "@/pages/test/test/Test";
 import TestResult from "@/pages/test/TestResult/TestResult";
 import BulletinBoard from "@/pages/board/BulletinBoard";
+import CardDetail from "@/pages/board/card/CardDetail";
 import Stats from "@/pages/stats/Stats";
 import StatsMbti from "@/pages/stats/statsMbti/StatsMbti";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <NotFound />,
-      children: [
-        { index: true, path: "/", element: <Home /> },
-        { path: "/test", element: <Test /> },
-        { path: "/result", element: <TestResult /> },
-        { path: "/board", element: <BulletinBoard /> },
-        { path: "/board/:mbti", element: <BulletinBoard /> },
-        { path: "/stats", element: <Stats /> },
-        { path: "/stats/:mbti", element: <StatsMbti /> }
-      ]
-    }
-  ]);
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "/", element: <Home /> },
+      { path: "/test", element: <Test /> },
+      { path: "/result", element: <TestResult /> },
+      { path: "/board", element: <BulletinBoard /> },
+      { path: "/board/:mbti", element: <BulletinBoard /> },
+      { path: "/board/cardDetail/:selectedId", element: <CardDetail /> },
+      { path: "/stats", element: <Stats /> },
+      { path: "/stats/:mbti", element: <StatsMbti /> }
+    ]
+  }
+]);
 
 export default router;
