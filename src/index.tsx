e66f15bc4f2afeaaa49b9bdc4de4fc75ed9ea307
[SW5] 문수민 <sum95771@gmail.com>
2023-10-05 20:46:17 +0900

    fix: 머지 오류 파일 develop브랜치파일로 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,45 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "@/index.css";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "@/pages/Home";
-import NotFound from "@/pages/NotFound";
-import Test from "@/pages/test/Test";
-import TestResult from "@/pages/test/TestResult";
-import BulletinBoard from "@/pages/board/BulletinBoard";
-import CardDetail from "@/pages/board/card/CardDetail";
-import Stats from "@/pages/stats/Stats";
-import StatsMbti from "@/pages/stats/StatsMbti";
-import BoardPost from "@/components/board/BoardPost";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, path: "/", element: <Home /> },
-      { path: "/test", element: <Test /> },
-      { path: "/result", element: <TestResult /> },
-      { path: "/board", element: <BulletinBoard /> },
-      { path: "/board/:mbti", element: <BulletinBoard /> },
-      { path: "/board/cardDetail/:selectedId", element: <CardDetail /> },
-      { path: "/stats", element: <Stats /> },
-      { path: "/stats/:mbti", element: <StatsMbti /> },
-    ],
-  },
-  {
-    // 게시글 작성 확인용 -> 추후에 BulletinBoard page 안에서 컴포넌트로만 사용할 예정
-    path: "/post",
-    element: (
-      <BoardPost
-        thisMbti={"ESTP"}
-        onThisClose={() => console.log("닫기")}
-        onThisComplete={(value) => console.log(value)}
-      />
-    ),
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./routers";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
