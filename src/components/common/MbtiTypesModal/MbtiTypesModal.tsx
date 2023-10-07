9dc591fb9c482046c8353bbd1f758fe0cf4bbb40
crud0626 <sisofiy6048@gmail.com>
2023-10-07 14:41:58 +0900

    Fix: MBTI 선택 모달 배경 부분 스타일 수정

--- src/components/common/MbtiTypesModal/MbtiTypesModal.tsx
+++ src/components/common/MbtiTypesModal/MbtiTypesModal.tsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-import { ModalBg, ModalWrap, MbtiButton, MbtiLabel, MbtiList, Toggle } from "./MbtiTypesModal.styles";
+import {
+  ModalBg,
+  ModalWrap,
+  MbtiButton,
+  MbtiLabel,
+  MbtiList,
+  Toggle
+} from "./MbtiTypesModal.styles";
 
-// MBTI 선택 모달
 const MbtiTypesModal = ({
   selectMbti,
   onThisMbti,
   isButton,
   onThisConfirm = () => {},
+  onCloseModal
 }: {
   selectMbti: string[];
   onThisMbti: (value: string[]) => void;
   isButton: boolean;
+  onCloseModal: (evt: React.MouseEvent<HTMLDivElement>) => void;
   onThisConfirm?: () => void;
 }) => {
   const [mbti_IE, setMbti_IE] = useState(selectMbti[0]);
@@ -23,137 +31,161 @@ const MbtiTypesModal = ({
   }, [mbti_IE, mbti_NS, mbti_FT, mbti_PJ, onThisMbti]);
 
   return (
-    <ModalWrap>
-      <h3 className="text-2xl font-black text-center">MBTI 선택</h3>
-      <ul>
-        <MbtiList>
-          <Toggle className={mbti_IE === "I" ? "left" : "right"} />
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_IE"
-              id="I"
-              className="hidden"
-              checked={mbti_IE === "I"}
-              onChange={(evt) => {
-                setMbti_IE(evt.target.id);
-              }}
-            />
-            <MbtiLabel htmlFor="I" className={mbti_IE === "I" ? "active" : ""}>
-              I
-            </MbtiLabel>
-          </div>
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_IE"
-              id="E"
-              className="hidden"
-              checked={mbti_IE === "E"}
-              onChange={(evt) => {
-                setMbti_IE(evt.target.id);
-              }}
-            />
-            <MbtiLabel htmlFor="E" className={mbti_IE === "E" ? "active" : ""}>
-              E
-            </MbtiLabel>
-          </div>
-        </MbtiList>
-        <MbtiList>
-          <Toggle className={mbti_NS === "N" ? "left" : "right"} />
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_NS"
-              id="N"
-              className="hidden"
-              checked={mbti_NS === "N"}
-              onChange={(evt) => setMbti_NS(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="N" className={mbti_NS === "N" ? "active" : ""}>
-              N
-            </MbtiLabel>
-          </div>
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_NS"
-              id="S"
-              className="hidden"
-              checked={mbti_NS === "S"}
-              onChange={(evt) => setMbti_NS(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="S" className={mbti_NS === "S" ? "active" : ""}>
-              S
-            </MbtiLabel>
-          </div>
-        </MbtiList>
-        <MbtiList>
-          <Toggle className={mbti_FT === "F" ? "left" : "right"} />
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_FT"
-              id="F"
-              className="hidden"
-              checked={mbti_FT === "F"}
-              onChange={(evt) => setMbti_FT(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="F" className={mbti_FT === "F" ? "active" : ""}>
-              F
-            </MbtiLabel>
-          </div>
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_FT"
-              id="T"
-              className="hidden"
-              checked={mbti_FT === "T"}
-              onChange={(evt) => setMbti_FT(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="T" className={mbti_FT === "T" ? "active" : ""}>
-              T
-            </MbtiLabel>
-          </div>
-        </MbtiList>
-        <MbtiList>
-          <Toggle className={mbti_PJ === "P" ? "left" : "right"} />
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_PJ"
-              id="P"
-              className="hidden"
-              checked={mbti_PJ === "P"}
-              onChange={(evt) => setMbti_PJ(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="P" className={mbti_PJ === "P" ? "active" : ""}>
-              P
-            </MbtiLabel>
-          </div>
-          <div className="flex-1 text-center">
-            <input
-              type="radio"
-              name="mbti_PJ"
-              id="J"
-              className="hidden"
-              checked={mbti_PJ === "J"}
-              onChange={(evt) => setMbti_PJ(evt.target.id)}
-            />
-            <MbtiLabel htmlFor="J" className={mbti_PJ === "J" ? "active" : ""}>
-              J
-            </MbtiLabel>
-          </div>
-        </MbtiList>
-      </ul>
-      {isButton && (
-        <MbtiButton onClick={() => onThisConfirm()}>확인</MbtiButton>
-      )}
-    </ModalWrap>
+    <ModalBg onClick={onCloseModal}>
+      <ModalWrap>
+        <h3 className="text-2xl font-black text-center">MBTI 선택</h3>
+        <ul>
+          <MbtiList>
+            <Toggle className={mbti_IE === "I" ? "left" : "right"} />
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_IE"
+                id="I"
+                className="hidden"
+                checked={mbti_IE === "I"}
+                onChange={(evt) => {
+                  setMbti_IE(evt.target.id);
+                }}
+              />
+              <MbtiLabel
+                htmlFor="I"
+                className={mbti_IE === "I" ? "active" : ""}
+              >
+                I
+              </MbtiLabel>
+            </div>
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_IE"
+                id="E"
+                className="hidden"
+                checked={mbti_IE === "E"}
+                onChange={(evt) => {
+                  setMbti_IE(evt.target.id);
+                }}
+              />
+              <MbtiLabel
+                htmlFor="E"
+                className={mbti_IE === "E" ? "active" : ""}
+              >
+                E
+              </MbtiLabel>
+            </div>
+          </MbtiList>
+          <MbtiList>
+            <Toggle className={mbti_NS === "N" ? "left" : "right"} />
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_NS"
+                id="N"
+                className="hidden"
+                checked={mbti_NS === "N"}
+                onChange={(evt) => setMbti_NS(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="N"
+                className={mbti_NS === "N" ? "active" : ""}
+              >
+                N
+              </MbtiLabel>
+            </div>
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_NS"
+                id="S"
+                className="hidden"
+                checked={mbti_NS === "S"}
+                onChange={(evt) => setMbti_NS(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="S"
+                className={mbti_NS === "S" ? "active" : ""}
+              >
+                S
+              </MbtiLabel>
+            </div>
+          </MbtiList>
+          <MbtiList>
+            <Toggle className={mbti_FT === "F" ? "left" : "right"} />
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_FT"
+                id="F"
+                className="hidden"
+                checked={mbti_FT === "F"}
+                onChange={(evt) => setMbti_FT(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="F"
+                className={mbti_FT === "F" ? "active" : ""}
+              >
+                F
+              </MbtiLabel>
+            </div>
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_FT"
+                id="T"
+                className="hidden"
+                checked={mbti_FT === "T"}
+                onChange={(evt) => setMbti_FT(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="T"
+                className={mbti_FT === "T" ? "active" : ""}
+              >
+                T
+              </MbtiLabel>
+            </div>
+          </MbtiList>
+          <MbtiList>
+            <Toggle className={mbti_PJ === "P" ? "left" : "right"} />
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_PJ"
+                id="P"
+                className="hidden"
+                checked={mbti_PJ === "P"}
+                onChange={(evt) => setMbti_PJ(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="P"
+                className={mbti_PJ === "P" ? "active" : ""}
+              >
+                P
+              </MbtiLabel>
+            </div>
+            <div className="flex-1 text-center">
+              <input
+                type="radio"
+                name="mbti_PJ"
+                id="J"
+                className="hidden"
+                checked={mbti_PJ === "J"}
+                onChange={(evt) => setMbti_PJ(evt.target.id)}
+              />
+              <MbtiLabel
+                htmlFor="J"
+                className={mbti_PJ === "J" ? "active" : ""}
+              >
+                J
+              </MbtiLabel>
+            </div>
+          </MbtiList>
+        </ul>
+        {isButton && (
+          <MbtiButton onClick={() => onThisConfirm()}>확인</MbtiButton>
+        )}
+      </ModalWrap>
+    </ModalBg>
   );
-}
+};
 
-export { ModalBg };
 export default MbtiTypesModal;
-
