a1b6049718a8bf5c90c6f48f73cf961f2e5039cc
rebi13 <rebi13@naver.com>
2023-10-22 15:18:41 +0900

    fix: 타입에 undefined 추가

--- src/components/test/TestResult/RelationType/RelationType.tsx
+++ src/components/test/TestResult/RelationType/RelationType.tsx
@@ -1,14 +1,26 @@
-import {Container, Title, GoodType, TypeTitle, Mbti, Contents, BadType } from "./RelationType.styles";
+import {
+  Container,
+  Title,
+  GoodType,
+  TypeTitle,
+  Mbti,
+  Contents,
+  BadType
+} from "./RelationType.styles";
 
 interface relation {
-  good: {
-    name: string;
-    description: string;
-  };
-  bad: {
-    name: string;
-    description: string;
-  };
+  good:
+    | undefined
+    | {
+        name: string | undefined;
+        description: string | undefined;
+      };
+  bad:
+    | undefined
+    | {
+        name: string | undefined;
+        description: string | undefined;
+      };
 }
 
 export default function RelationType({ good, bad }: relation) {
@@ -17,13 +29,13 @@ export default function RelationType({ good, bad }: relation) {
       <Title>유형별 관계성</Title>
       <GoodType>
         <TypeTitle>잘 어울리는 유형</TypeTitle>
-        <Mbti>❤️{good.name}❤️</Mbti>
-        <Contents>{good.description}</Contents>
+        <Mbti>❤️{good?.name}❤️</Mbti>
+        <Contents>{good?.description}</Contents>
       </GoodType>
       <BadType>
         <TypeTitle>친해지기 힘든 유형</TypeTitle>
-        <Mbti>❄️{bad.name}❄️</Mbti>
-        <Contents>{bad.description}</Contents>
+        <Mbti>❄️{bad?.name}❄️</Mbti>
+        <Contents>{bad?.description}</Contents>
       </BadType>
     </Container>
   );
