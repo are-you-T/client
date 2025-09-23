9e4158f7d5b3845b2e90259d342eff3e0d9efba5
rebi13 <rebi13@naver.com>
2025-09-24 00:25:12 +0900

    Feature: 시간 변환함수 추가

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 /**
  * 작성자명 : 원종석
  * 작성일자 : 2023-08-28 (월)
@@ -6,13 +8,13 @@
  * @returns "YYYY-MM-DD" 형태의 string 데이터
  */
 const formatDateToString = (date: Date | string): string => {
-    if (typeof date === "string") date = new Date(date);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1을 하고 2자리로 포맷
-    const day = String(date.getDate()).padStart(2, "0"); // 일자를 2자리로 포맷
+  if (typeof date === "string") date = new Date(date);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1을 하고 2자리로 포맷
+  const day = String(date.getDate()).padStart(2, "0"); // 일자를 2자리로 포맷
 
-    const formattedDate = `${year}-${month}-${day}`;
-    return formattedDate;
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
 };
 
 /**
@@ -23,15 +25,26 @@ const formatDateToString = (date: Date | string): string => {
  * @returns "YYYY-MM-DD" 형태의 string 데이터
  */
 const setKoreaTime = (date: Date): string => {
-    const utcDate: Date = new Date(date);
+  const utcDate: Date = new Date(date);
 
-    // 한국 시간으로 변환
-    const koreaTimeOffset: number = 9 * 60 * 60 * 1000; // 한국은 UTC+9
-    const koreaDate: Date = new Date(utcDate.getTime() + koreaTimeOffset);
+  // 한국 시간으로 변환
+  const koreaTimeOffset: number = 9 * 60 * 60 * 1000; // 한국은 UTC+9
+  const koreaDate: Date = new Date(utcDate.getTime() + koreaTimeOffset);
 
-    // 한국 시간을 원하는 형식으로 포맷
-    const formattedDate = formatDateToString(koreaDate);
-    return formattedDate;
+  // 한국 시간을 원하는 형식으로 포맷
+  const formattedDate = formatDateToString(koreaDate);
+  return formattedDate;
 };
 
-export { formatDateToString, setKoreaTime };
+/**
+ * 작성자명 : 원종석
+ * 작성일자 : 2025-08-28 (월)
+ * 작성내용 : ISO 형식의 Date String을 다음과 같은 형태로 변환한다.
+ * @param {Date} date 날짜
+ * @returns "YYYY-MM-DD HH:mm:ss" 형태의 string 데이터
+ */
+const setDayjsFormatString = (date?: string): string => {
+  return dayjs(date).format("YYYY-MM-DD HH:mm:ss");
+};
+
+export { formatDateToString, setKoreaTime, setDayjsFormatString };
