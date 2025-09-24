f130a5a0446455468568d5465fd2e289bb37c004
rebi13 <rebi13@naver.com>
2025-09-24 22:47:46 +0900

    Remove: 파일 제거

--- src/components/common/LoadingIndicator/LoadingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-import { PageLoading, LoadingIcon } from "./LoadingIndicator.styles";
-
-export default function LoadingIndicator() {
-  return (
-    <PageLoading>
-      <LoadingIcon />
-    </PageLoading>
-  );
-}
