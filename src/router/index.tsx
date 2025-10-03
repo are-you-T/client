eec62fb9eab85b88ae87741a11561da3f94fe58a
rebi13 <rebi13@naver.com>
2025-10-03 14:13:31 +0900

    Feature: 문항 단건 조회 페이지 라우터

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,7 +7,7 @@ const HomePage = lazy(() => import("@/pages/Home"));
 const MemoPage = lazy(() => import("@/pages/Memo"));
 const MemoViewPage = lazy(() => import("@/pages/Memo/View"));
 const QuestionPage = lazy(() => import("@/pages/Question"));
-// const QuestionViewPage = lazy(() => import("@/pages/Question/View"));
+const QuestionViewPage = lazy(() => import("@/pages/Question/View"));
 // const StatsPage = lazy(() => import("@/pages/Stats/Stats"));
 // const StatsMbtiPage = lazy(() => import("@/pages/StatsMbti/StatsMbti"));
 const NotFoundPage = lazy(() => import("@/pages/NotFound"));
@@ -17,7 +17,7 @@ const routePaths = [
   { path: "/memo", element: <MemoPage /> },
   { path: "/memo/:id", element: <MemoViewPage /> },
   { path: "/question", element: <QuestionPage /> },
-  // { path: "/question/:id", element: <QuestionViewPage /> },
+  { path: "/question/:id", element: <QuestionViewPage /> },
   // { path: "/stats", element: <StatsPage /> },
   // { path: "/stats/:mbti", element: <StatsMbtiPage /> },
   // { path: "/test", element: <TestPage /> },
