f5a857bf662537b373fd7a8b5f335e4cdad1c372
rebi13 <rebi13@naver.com>
2025-10-07 22:27:45 +0900

    Remove: 사용하지 않는 파일 제거 (정리)

--- src/components/test/TestResult/HashTag/HashTag.tsx
+++
@@ -1,9 +0,0 @@
-import { Tag } from "./HashTag.styles";
-
-interface HashTagProps {
-  text: string | undefined;
-}
-
-export default function HashTag({ text }: HashTagProps) {
-  return <Tag>#{text}</Tag>;
-}
